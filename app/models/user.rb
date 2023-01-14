834939de19fcb098f1ddc70b9a92004414c15d6e
tyo6ra6 <maako527@icloud.com>
2023-01-14 17:58:12 +0900

    コメント投稿機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          has_many :posts
+         has_many :comments
 
          validates :nickname, presence: true
          validates :birth_day, presence: true
